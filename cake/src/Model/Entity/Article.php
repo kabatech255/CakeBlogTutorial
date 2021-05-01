5fafa00d6b873e3fa698d342c5dc7e6106a8de46
rk1quol <kmrrt@rknoMacBook-Pro.local>
2021-05-01 21:02:32 +0900

    タグによる絞り込み機能

--- cake/src/Model/Entity/Article.php
+++ cake/src/Model/Entity/Article.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
@@ -17,22 +18,22 @@ use Cake\ORM\Entity;
  */
 class Article extends Entity
 {
-    /**
-     * Fields that can be mass assigned using newEntity() or patchEntity().
-     *
-     * Note that when '*' is set to true, this allows all unspecified fields to
-     * be mass assigned. For security purposes, it is advised to set '*' to false
-     * (or remove it), and explicitly make individual fields accessible as needed.
-     *
-     * @var array
-     */
-    protected $_accessible = [
-        'title' => true,
-        'body' => true,
-        'category_id' => true,
-        'user_id' => true,
-        'created' => true,
-        'modified' => true,
-        'categories' => true,
-    ];
+  /**
+   * Fields that can be mass assigned using newEntity() or patchEntity().
+   *
+   * Note that when '*' is set to true, this allows all unspecified fields to
+   * be mass assigned. For security purposes, it is advised to set '*' to false
+   * (or remove it), and explicitly make individual fields accessible as needed.
+   *
+   * @var array
+   */
+  protected $_accessible = [
+    'title' => true,
+    'body' => true,
+    'category_id' => true,
+    'user_id' => true,
+    'created' => true,
+    'modified' => true,
+    'categories' => true,
+  ];
 }
--- cake/src/Template/Articles/add.ctp
+++ cake/src/Template/Articles/add.ctp
@@ -4,6 +4,10 @@ echo $this->Form->create($article);
 echo $this->Form->control('title');
 echo $this->Form->input('user_id', ['type' => 'hidden', 'value' => $this->Auth->user('id')]);
 echo $this->Form->control('body', ['rows' => '3']);
+
+/**
+ * $this->request->getData()['categories']['_ids'] = [1, 2, ...]
+ */
 echo $this->Form->select('categories._ids', $categories, [
   'multiple' => 'checkbox',
 ]);
